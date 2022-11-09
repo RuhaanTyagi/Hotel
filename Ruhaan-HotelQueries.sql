1d03bca842b5f0bd17178331da7edaedc33637e8
RuhaanTyagi <46258972+RuhaanTyagi@users.noreply.github.com>
2022-11-08 21:52:47 -0500

    Update Ruhaan-HotelQueries.sql

--- Ruhaan-HotelQueries.sql
+++ Ruhaan-HotelQueries.sql
@@ -1,9 +1,9 @@
 USE HotelReservation;
 
 /*
--- Write a query that returns a list of reservations that end in July 2023,
+-- A query that returns a list of reservations that end in July 2023,
  including the name of the guest, the room number(s), and the reservation dates.
--- 4 row
+--
 
 */
 SELECT 
@@ -19,10 +19,10 @@ WHERE Reservation.EndDate BETWEEN '2023-07-01' AND '2023-07-31'
  ;
 
 /*
-Write a query that returns a list of all reservations for rooms with a jacuzzi, 
+A query that returns a list of all reservations for rooms with a jacuzzi, 
 displaying the guest's name, the room number, and the dates of the reservation.
 
--- 11 rows
+
 */
 SELECT 
 	CONCAT(Guest.FirstNAme,' ', Guest.LastName) as GuestName,
@@ -41,11 +41,11 @@ WHERE Amenity.AmenityId = 4
 
 
 /*
-Write a query that returns all the rooms reserved for a specific guest, including the guest's name, the room(s) reserved,
+A query that returns all the rooms reserved for a specific guest, including the guest's name, the room(s) reserved,
  the starting date of the reservation,
  and how many people were included in the reservation. (Choose a guest's name from the existing data.)
  
- -- 2 rows
+
 */
 SELECT 
 	CONCAT(Guest.FirstNAme,' ', Guest.LastName) as GuestName,
@@ -60,7 +60,7 @@ WHERE Guest.FirstNAme = 'Hafsa' AND Guest.LastName = 'Degmaster'
  ;
  
  /*
- 4. Write a query that returns a list of rooms, reservation ID, and per-room cost for each reservation. 
+ 4. A query that returns a list of rooms, reservation ID, and per-room cost for each reservation. 
  The results should include all rooms, whether or not there is a reservation associated with the room.
  -- 27 rows
  */
@@ -74,7 +74,7 @@ WHERE Guest.FirstNAme = 'Hafsa' AND Guest.LastName = 'Degmaster'
  LEFT JOIN Reservation ON Reservation.ReservationId = GuestRoomReservation.ReservationId;
  
  /*
- 5. Write a query that returns all the rooms accommodating at least three guests and that are reserved on any date in April 2023.
+ 5. A query that returns all the rooms accommodating at least three guests and that are reserved on any date in April 2023.
  
  -- 12 rows
  */
@@ -96,7 +96,7 @@ HAVING total >= 3 AND Reservation.EndDate BETWEEN '2023-04-01' AND '2023-04-31'
 ;
 
 /*
-6. Write a query that returns a list of all guest names and the number of reservations per guest,
+6. A query that returns a list of all guest names and the number of reservations per guest,
  sorted starting with the guest with the most reservations and then by the guest's last name.
  
  -- 12 rows
@@ -111,7 +111,7 @@ HAVING total >= 3 AND Reservation.EndDate BETWEEN '2023-04-01' AND '2023-04-31'
  ORDER BY reservationNumber DESC, Guest.LastName;
  
 /*
-7. Write a query that displays the name, address, and phone number of a guest based on their phone number.
+7. A query that displays the name, address, and phone number of a guest based on their phone number.
  (Choose a phone number from the existing data.)
  
  1 row
